0c3395d2e92bce6d9fc837425f7656835fc91f95
KALIMI <karenmirakyan@gmail.com>
2020-12-02 12:26:38 +0400

    Initial Commit

---
+++ vFruits.am/vFruits_amApp.swift
@@ -0,0 +1,17 @@
+//
+//  vFruits_amApp.swift
+//  vFruits.am
+//
+//  Created by Karen Mirakyan on 02.12.20.
+//
+
+import SwiftUI
+
+@main
+struct vFruits_amApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
